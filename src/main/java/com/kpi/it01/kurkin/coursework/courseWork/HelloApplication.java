ea1a1d52b3fbade4fe1c5014661716c96cf15bee
KurKing <lyosha.kurkin@gmail.com>
2021-03-21 20:59:34 +0200

    Create UI for log in and sign up

--- src/main/java/com/kpi/it01/kurkin/coursework/courseWork/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.kpi.it01.kurkin.coursework.courseWork;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/com/kpi/it01/kurkin/coursework/courseWork/LoginServletController.java
@@ -0,0 +1,25 @@
+package com.kpi.it01.kurkin.coursework.courseWork;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServletController", value = "/login")
+public class LoginServletController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    private void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+        dispatcher.forward(request, response);
+    }
+}
