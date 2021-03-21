3923915ec661cfd0206f786be3a95ae3b286a35f
KurKing <lyosha.kurkin@gmail.com>
2021-03-21 11:29:19 +0200

    Initial commit

---
+++ src/main/java/com/kpi/it01/kurkin/coursework/courseWork/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.kpi.it01.kurkin.coursework.courseWork;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
